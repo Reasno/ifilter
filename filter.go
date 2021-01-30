c4092ffb75635aaedf7cd79b04af263064bdf86e
reasno <guxi99@gmail.com>
2021-01-31 01:35:40 +0800

    feat: add filterSlice and filter

--- filter.go
+++ filter.go
@@ -30,7 +30,7 @@ type Option interface {
 // along with any invalid arguments errors raised while filtering.
 //
 //
-func (c Collection) Filter(callback interface{}, Opts... Option) error {
+func (c Collection) FilterSlice(callback interface{}, Opts ...Option) error {
 	cType := reflect.TypeOf(callback)
 	if cType == nil {
 		return errors.New("callback must not be an untyped nil")
@@ -66,6 +66,52 @@ func (c Collection) Filter(callback interface{}, Opts... Option) error {
 	if last := outVals[len(outVals)-1]; isError(last.Type()) {
 		if err, _ := last.Interface().(error); err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+// Filter examines every element in the collection and callback
+// with elements that implement the desired interface. The callback
+// argument must be a valid function with the given signature:
+//
+// func(i I) error
+//
+// "I" must be a valid interface, such as io.reader or error.
+//
+// The callback will be fired for every element implementing I.
+// The callback can optionally return an error. If so, the error
+// will be relayed to the return value of Filter,
+// along with any invalid arguments errors raised while filtering.
+//
+func (c Collection) Filter(callback interface{}, Opts ...Option) error {
+	cType := reflect.TypeOf(callback)
+	if cType == nil {
+		return errors.New("callback must not be an untyped nil")
+	}
+	if cType.Kind() != reflect.Func {
+		return fmt.Errorf("callback must be a function, got %v (type %v)", callback, cType)
+	}
+	if cType.NumIn() != 1 {
+		return fmt.Errorf("callback must have exactly 1 argument, got %d", cType.NumIn())
+	}
+	argType := cType.In(0)
+	if argType.Kind() != reflect.Interface {
+		return errors.New("argument in callback must be a slice of interface")
+	}
+
+	for i := range c {
+		if !implements(c[i], argType) {
+			continue
+		}
+		outVals := reflect.ValueOf(callback).Call([]reflect.Value{reflect.ValueOf(c[i])})
+		if len(outVals) == 0 {
+			return nil
+		}
+		if last := outVals[len(outVals)-1]; isError(last.Type()) {
+			if err, _ := last.Interface().(error); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
--- filter_test.go
+++ filter_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-type mock struct {}
+type mock struct{}
 
 func (mock) f() {}
 
@@ -14,16 +14,16 @@ type i interface {
 	f()
 }
 
-type mock2 struct {}
+type mock2 struct{}
 
-func (mock2) f() {}
+func (mock2) f()  {}
 func (mock2) f2() {}
 
-func TestCollection_Filter(t *testing.T) {
-	cases := []struct{
-		name string
-		data []interface{}
-		target interface{}
+func TestCollection_FilterSlice(t *testing.T) {
+	cases := []struct {
+		name      string
+		data      []interface{}
+		target    interface{}
 		shouldErr bool
 	}{
 		{
@@ -33,7 +33,7 @@ func TestCollection_Filter(t *testing.T) {
 				if len(filtered) != 1 {
 					t.Fatal()
 				}
-				if ! reflect.DeepEqual(filtered[0],mock{}) {
+				if !reflect.DeepEqual(filtered[0], mock{}) {
 					t.Fatal("mock is not the same")
 				}
 			},
@@ -123,11 +123,130 @@ func TestCollection_Filter(t *testing.T) {
 		c := cc
 		t.Run(c.name, func(t *testing.T) {
 			col := Collection(c.data)
+			err := col.FilterSlice(c.target)
+			if c.shouldErr && err == nil {
+				t.Fatal("should err, but got none")
+			}
+			if !c.shouldErr && err != nil {
+				t.Fatalf("should not err, but got %s", err)
+			}
+		})
+	}
+}
+
+func TestCollection_Filter(t *testing.T) {
+	cases := []struct {
+		name      string
+		data      []interface{}
+		target    interface{}
+		shouldErr bool
+	}{
+		{
+			"basic",
+			[]interface{}{mock{}, struct{}{}},
+			func(filtered i) {
+				if !reflect.DeepEqual(filtered, mock{}) {
+					t.Fatal("mock is not the same")
+				}
+			},
+			false,
+		},
+		{
+			"none",
+			[]interface{}{struct{}{}},
+			func(filtered i) {
+				t.Fatal()
+			},
+			false,
+		},
+		{
+			"all",
+			[]interface{}{mock{}, mock{}},
+			func() func(i) error {
+				time := 0
+				return func(filtered i) error {
+					time++
+					if time == 2 {
+						return errors.New("2")
+					}
+					return nil
+				}
+			}(),
+			true,
+		},
+		{
+			"empty",
+			[]interface{}{},
+			func(filtered i) {
+				t.Fatal()
+
+			},
+			false,
+		},
+		{
+			"two mock of different type",
+			[]interface{}{mock{}, mock2{}},
+			func() func(i) error {
+				time := 0
+				return func(filtered i) error {
+					time++
+					if time == 2 {
+						return errors.New("2")
+					}
+					return nil
+				}
+			}(),
+			true,
+		},
+		{
+			"return err from callback",
+			[]interface{}{},
+			func(filtered i) error {
+				return errors.New("err")
+			},
+			false,
+		},
+		{
+			"not valid interface",
+			[]interface{}{mock{}, struct{}{}},
+			func(filtered []mock) {},
+			true,
+		},
+		{
+			"not valid interface",
+			[]interface{}{mock{}, struct{}{}},
+			func(filtered int) {},
+			true,
+		},
+		{
+			"not valid interface",
+			[]interface{}{mock{}, struct{}{}},
+			func() {},
+			true,
+		},
+		{
+			"not valid interface",
+			[]interface{}{mock{}, struct{}{}},
+			1,
+			true,
+		},
+		{
+			"not valid interface",
+			[]interface{}{mock{}, struct{}{}},
+			nil,
+			true,
+		},
+	}
+
+	for _, cc := range cases {
+		c := cc
+		t.Run(c.name, func(t *testing.T) {
+			col := Collection(c.data)
 			err := col.Filter(c.target)
 			if c.shouldErr && err == nil {
 				t.Fatal("should err, but got none")
 			}
-			if ! c.shouldErr && err != nil {
+			if !c.shouldErr && err != nil {
 				t.Fatalf("should not err, but got %s", err)
 			}
 		})
